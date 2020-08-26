016f699b7cc9ebc5a7aebcb37ed65e4e52191245
ZHLix <15127441165@163.com>
2020-08-26 15:50:58 +0800

    添加 demo

---
+++ demo.js
@@ -0,0 +1,7 @@
+/*
+ * @Date: 2020-08-26 15:48:06
+ * @LastEditors: zhlix <15127441165@163.com>
+ * @LastEditTime: 2020-08-26 15:48:12
+ * @FilePath: \test\demo.js
+ */
+console.log('this is test');
